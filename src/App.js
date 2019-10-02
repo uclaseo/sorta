383b4a18fae23cc17c910b710185939dc9eb0dbe
Inseok Seo <inseok.ucla@gmail.com>
2019-10-02 10:37:23 -0700

    Move states to App

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React from 'react';
 import BarContainer from './BarContainer';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>hello world</div>
-        <BarContainer/>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    numbers: [500, 100, 400, 10, 40, 220, 600],
+  }
+
+  start = () => {
+    this.setState(prevState => {
+      const newNumbers = prevState.numbers.reverse();
+      return {
+        numbers: newNumbers,
+      };
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>hello world</div>
+          <BarContainer numbers={this.state.numbers} />
+          <button onClick={this.start}>click me</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

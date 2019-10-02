96dbba17437d225649c18600deeb62780d6b7215
Inseok Seo <inseok.ucla@gmail.com>
2019-10-01 22:02:13 -0700

    Fixed some css

--- src/BarContainer.js
+++ src/BarContainer.js
@@ -3,7 +3,7 @@ import Bar from './Bar';
 import './BarContainer.css';
 
 function BarContainer() {
-  const dummyBars = [1, 3, 4, 10, 22, 50, 3, 4, 10, 22, 50, 3, 4, 10, 22, 50, 3, 4, 10, 22, 50, 3, 4, 10, 22, 50, 3, 4, 10, 22, 50, 3, 4, 10, 22, 50, 3, 4, 10, 22, 50];
+  const dummyBars = [1, 3, 4, 4, 10, 22, 50];
   return (
     <div className="barContainer">
       {dummyBars.map((value) => {

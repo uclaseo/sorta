907e713b3c43b45b8716a3fbbe5995fac317dd90
Inseok Seo <inseok.ucla@gmail.com>
2019-10-24 16:35:48 -0700

    Added bublesort

--- src/containers/Visualizer/Visualizer.js
+++ src/containers/Visualizer/Visualizer.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import { getMergeSortAnimations } from '../../helpers/mergeSort';
+import { bubbleSort } from '../../helpers/bubbleSort';
 import styles from './Visualizer.module.css';
 
 
@@ -8,16 +11,21 @@ class Visualizer extends Component {
   }
 
   componentDidMount() {
-    this.generateRandomArray(100);
+    this.generateRandomArray(50);
   }
 
   generateRandomArray = (length) => {
     const array = [];
     for (let i = 0; i < length; i += 1) {
-      array.push(this.generateRandomIntFromInterval(10, length));
+      array.push(this.generateRandomIntFromInterval(10, 100));
     }
     this.setState({
       array,
+    }, () => {
+      for (let i = 0; i < array.length; i += 1) {
+        const bar = document.getElementsByClassName(styles.barValue);
+        bar[i].style.backgroundColor = 'white';
+      }
     });
   }
 
@@ -25,8 +33,62 @@ class Visualizer extends Component {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  handleOnClick = () => {
-    this.generateRandomArray(100);
+  handleNew = () => {
+    this.generateRandomArray(50);
+  }
+
+  handleMergeSort = () => {
+    const animations = getMergeSortAnimations(this.state.array);
+    // console.log(animations);
+    // for (let i = 0; i < animations.length; i += 1) {
+    //   const bars = document.getElementsByClassName(styles.barValue);
+    //   const isColorChange = i % 3 !== 2;
+    //   if (isColorChange) {
+    //     const [barOneIdx, barTwoIdx] = animations[i];
+    //     const barOneStyle = bars[barOneIdx].style;
+    //     const barTwoStyle = bars[barTwoIdx].style;
+    //     const color = i % 3 === 0 ? 'yellow' : 'red';
+    //     setTimeout(() => {
+    //       barOneStyle.backgroundColor = color;
+    //       barTwoStyle.backgroundColor = color;
+    //     }, i * 1000);
+    //   } else {
+    //     setTimeout(() => {
+    //       const [barOneIdx, newHeight] = animations[i];
+    //       const barOneStyle = bars[barOneIdx].style;
+    //       barOneStyle.height = `${newHeight}px`;
+    //     }, i * 1000);
+    //   }
+    // }
+  }
+
+  handleSelectSort = () => {
+
+  }
+
+  handleBubbleSort = () => {
+    const animations = bubbleSort(this.state.array, []);
+    for (let i = 0; i < animations.length; i += 1) {
+      const bars = document.getElementsByClassName(styles.barValue);
+      const [barOneIndex, barTwoIndex, swap] = animations[i];
+      const barOneStyle = bars[barOneIndex].style;
+      const barTwoStyle = bars[barTwoIndex].style;
+      if (!swap) {
+        setTimeout(() => {
+          if (bars[barOneIndex - 1]) {
+            bars[barOneIndex - 1].style.backgroundColor = 'white';
+          }
+          barOneStyle.backgroundColor = 'green';
+          barTwoStyle.backgroundColor = 'green';
+        }, 10 * i);
+      } else {
+        setTimeout(() => {
+          const temp = barTwoStyle.height;
+          barTwoStyle.height = barOneStyle.height;
+          barOneStyle.height = temp;
+        }, 10 * i);
+      }
+    }
   }
 
   render() {
@@ -45,10 +107,10 @@ class Visualizer extends Component {
             ))
           }
         </div>
-        <button onClick={this.handleOnClick}>New Numbers</button>
-        <button onClick={this.handleOnClick}>Merge</button>
-        <button onClick={this.handleOnClick}>Select</button>
-        <button onClick={this.handleOnClick}>Insertion</button>
+        <button onClick={this.handleNew}>New Numbers</button>
+        <button onClick={this.handleMergeSort}>Merge</button>
+        <button onClick={this.handleSelectSort}>Select</button>
+        <button onClick={this.handleBubbleSort}>Bubble</button>
       </div>
     );
   }
--- src/helpers/mergeSort.js
+++ src/helpers/mergeSort.js
@@ -1,23 +1,147 @@
-const mergeSort = (array) => {
+// export function getMergeSortAnimations(array) {
+//   const animations = [];
+//   if (array.length <= 1) return array;
+//   const auxiliaryArray = array.slice();
+//   mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
+//   console.log('animations', animations);
+//   return animations;
+// }
+// // let i = 0;
+// function mergeSortHelper(
+//   mainArray,
+//   startIdx,
+//   endIdx,
+//   auxiliaryArray,
+//   animations,
+// ) {
+//   if (startIdx === endIdx) return;
+//   // console.log('i', i);
+//   // console.log('startIndex', startIdx);
+//   // console.log('endIdx', endIdx);
+//   // console.log('auxilaryArray', auxiliaryArray);
+//   // console.log('mainArray', mainArray);
+//   const middleIdx = Math.floor((startIdx + endIdx) / 2);
+//   // i += 1;
+//   mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+//   mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+//   // console.log('=============')
+//   doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+// }
+
+// function doMerge(
+//   mainArray,
+//   startIdx,
+//   middleIdx,
+//   endIdx,
+//   auxiliaryArray,
+//   animations,
+// ) {
+//   let k = startIdx;
+//   let i = startIdx;
+//   let j = middleIdx + 1;
+//   while (i <= middleIdx && j <= endIdx) {
+//     // These are the values that we're comparing; we push them once
+//     // to change their color.
+//     animations.push([i, j]);
+//     // These are the values that we're comparing; we push them a second
+//     // time to revert their color.
+//     animations.push([i, j]);
+//     if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+//       // We overwrite the value at index k in the original array with the
+//       // value at index i in the auxiliary array.
+//       animations.push([k, auxiliaryArray[i]]);
+//       mainArray[k++] = auxiliaryArray[i++];
+//     } else {
+//       // We overwrite the value at index k in the original array with the
+//       // value at index j in the auxiliary array.
+//       animations.push([k, auxiliaryArray[j]]);
+//       mainArray[k++] = auxiliaryArray[j++];
+//     }
+//   }
+//   while (i <= middleIdx) {
+//     // These are the values that we're comparing; we push them once
+//     // to change their color.
+//     animations.push([i, i]);
+//     // These are the values that we're comparing; we push them a second
+//     // time to revert their color.
+//     animations.push([i, i]);
+//     // We overwrite the value at index k in the original array with the
+//     // value at index i in the auxiliary array.
+//     animations.push([k, auxiliaryArray[i]]);
+//     mainArray[k++] = auxiliaryArray[i++];
+//   }
+//   while (j <= endIdx) {
+//     // These are the values that we're comparing; we push them once
+//     // to change their color.
+//     animations.push([j, j]);
+//     // These are the values that we're comparing; we push them a second
+//     // time to revert their color.
+//     animations.push([j, j]);
+//     // We overwrite the value at index k in the original array with the
+//     // value at index j in the auxiliary array.
+//     animations.push([k, auxiliaryArray[j]]);
+//     mainArray[k++] = auxiliaryArray[j++];
+//   }
+// }
+
+export function getMergeSortAnimations(array) {
+  const animations = [];
+  const sorted = mergeSort(array, animations);
+  console.log('sorted', sorted);
+  console.log('animations', animations)
+  return animations;
+}
+
+
+const mergeSort = (array, animations, originalArray, fakeArray) => {
   if (array.length === 1) return array;
   const half = Math.floor(array.length / 2);
-  const firstHalf = mergeSort(array.slice(0, half));
-  const secondHalf = mergeSort(array.slice(half, array.length));
+  const firstHalf = mergeSort(array.slice(0, half), animations, originalArray, fakeArray);
+  const secondHalf = mergeSort(array.slice(half, array.length), animations, originalArray, fakeArray);
   let i = 0;
   let j = 0;
   const sorted = [];
   while (i < firstHalf.length && j < secondHalf.length) {
+    // console.log(animations)
+    // animations.push([i, j])
     if (firstHalf[i] < secondHalf[j]) {
+      const index = originalArray.indexOf(firstHalf[i]);
+      animations.push(index);
       sorted.push(firstHalf[i]);
       i += 1;
     } else {
+      const index = originalArray.indexOf(secondHalf[i]);
+      animations.push(index);
       sorted.push(secondHalf[j]);
       j += 1;
     }
   }
-  sorted.push(...firstHalf.slice(i))
-  sorted.push(...secondHalf.slice(j));
 
+  if (i === firstHalf.length) {
+    const index = originalArray.indexOf(firstHalf[i]);
+    animations.push(index);
+    sorted.push(...secondHalf.slice(j));
+  } else if (j === secondHalf.length) {
+    const index = originalArray.indexOf(firstHalf[i]);
+    animations.push(index);
+    sorted.push(...firstHalf.slice(i));
+  }
+  console.log(sorted);
   return sorted;
 }
+
+
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 1; j < array.length; j += 1) {
+    }
+  }
+}
+
+
 export default mergeSort;
+const array = [9, 8, 7, 2]
+const animations = [];
+// console.log(mergeSort(array, animations, array, array))
+// console.log(animations);

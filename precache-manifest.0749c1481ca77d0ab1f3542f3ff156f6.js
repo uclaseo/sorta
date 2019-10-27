486a4764d317acd76c42f9863e3618061c1048be
Inseok Seo <inseok.ucla@gmail.com>
2019-10-26 18:15:21 -0700

    Updates

---
+++ precache-manifest.0749c1481ca77d0ab1f3542f3ff156f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fea9273fc9ccee7c8e8095d9fabfba1",
+    "url": "/sorta/index.html"
+  },
+  {
+    "revision": "8e8d58f55568c2e10888",
+    "url": "/sorta/static/css/main.6017288c.chunk.css"
+  },
+  {
+    "revision": "1a3b6c84c9a55d13348d",
+    "url": "/sorta/static/js/2.b8721068.chunk.js"
+  },
+  {
+    "revision": "8e8d58f55568c2e10888",
+    "url": "/sorta/static/js/main.ccd324e9.chunk.js"
+  },
+  {
+    "revision": "60a055716f068e75780a",
+    "url": "/sorta/static/js/runtime-main.3c31a4b4.js"
+  }
+]);
